03107d260b168a2b598358eb5f557a42e0ae2b2c
spalenzam <spalenzam@gmail.com>
2022-05-04 21:57:34 -0300

    Modificación para mostrar imagen

--- BuenSabor/src/main/java/com/buenSabor/controllers/ArticuloManufacturadoController.java
+++ BuenSabor/src/main/java/com/buenSabor/controllers/ArticuloManufacturadoController.java
@@ -1,18 +1,26 @@
 package com.buenSabor.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
+
 
 import javax.validation.Valid;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.buenSabor.entity.ArticuloManufacturado;
 import com.buenSabor.services.ArticuloManufacturadoService;
@@ -22,6 +30,51 @@ import com.commons.controllers.CommonController;
 @RequestMapping(path="api/buensabor/articulosmanufacturados")
 public class ArticuloManufacturadoController extends CommonController<ArticuloManufacturado, ArticuloManufacturadoService>{
 	
+	@GetMapping("/uploads/img/{id}")
+	public ResponseEntity<?> verImagen(@PathVariable Long id){
+		
+		Optional<ArticuloManufacturado> o = service.findById(id);
+		if(o.isEmpty() || o.get().getImagen() == null ){
+			return ResponseEntity.notFound().build();
+		}
+		
+		Resource imagen = new ByteArrayResource(o.get().getImagen());
+		
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imagen);
+		
+	}
+	
+	@PostMapping("/crear-con-imagen")
+	public ResponseEntity<?> crearConImagen(@Valid ArticuloManufacturado articuloManufacturado, BindingResult result, @RequestParam MultipartFile archivo) throws IOException {
+		
+		if(!archivo.isEmpty()){
+			articuloManufacturado.setImagen(archivo.getBytes());
+		}
+		
+		return super.crear(articuloManufacturado, result);
+	}
+	
+	@PutMapping("/editar-con-imagen/{id}")
+	public ResponseEntity<?> editarConImagen (@Valid ArticuloManufacturado artmanufacturado, BindingResult result, @PathVariable Long id, @RequestParam MultipartFile archivo) throws IOException{
+		
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}		
+		 		
+		Optional<ArticuloManufacturado> o = service.findById(id);
+		if(o.isEmpty()){
+			return ResponseEntity.notFound().build();
+		}
+		
+		ArticuloManufacturado artmanufacturadoDB = o.get();
+		artmanufacturadoDB.setPrecioVenta(artmanufacturado.getPrecioVenta());
+		if(!archivo.isEmpty()){
+			artmanufacturadoDB.setImagen(archivo.getBytes());
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(artmanufacturadoDB));
+	}
+
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar (@Valid @RequestBody ArticuloManufacturado artmanufacturado, BindingResult result, @PathVariable Long id){
 		
