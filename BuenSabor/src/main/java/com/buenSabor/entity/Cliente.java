92ae61151e06a8cb976b06d9dc56a74963157373
spalenzam <spalenzam@gmail.com>
2022-05-23 23:09:57 -0300

    Creación endpoint para crud

--- BuenSabor/src/main/java/com/buenSabor/entity/Cliente.java
+++ BuenSabor/src/main/java/com/buenSabor/entity/Cliente.java
@@ -10,9 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="cliente")
