06f7a8764eab69b269d98c4c0f5aebaac710797d
MaarPotichkin <63211633+MaarPotichkin@users.noreply.github.com>
2022-06-03 12:06:38 -0300

    endpoint mercado pago

--- BuenSabor/src/main/java/com/buenSabor/services/MercadoPagoDatosServiceImpl.java
+++ BuenSabor/src/main/java/com/buenSabor/services/MercadoPagoDatosServiceImpl.java
@@ -1,12 +1,19 @@
 package com.buenSabor.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.buenSabor.entity.MercadoPagoDatos;
 import com.buenSabor.repository.MercadoPagoDatosRepository;
 import com.commons.services.CommonServiceImpl;
 
+
 @Service
 public class MercadoPagoDatosServiceImpl extends CommonServiceImpl<MercadoPagoDatos, MercadoPagoDatosRepository> implements MercadoPagoDatosService {
 
+	@Autowired
+	protected MercadoPagoDatosRepository mercadoPagoDatosRepository;
+	
+	
+	 
 }
